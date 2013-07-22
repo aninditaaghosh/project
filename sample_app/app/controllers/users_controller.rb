53ccb23f2b369c16d7f58f10042cc7f6e65a1bb4
aninditaaghosh <aninditaaghosh@gmail.com>
2013-07-23 03:00:31 +0530

    error in micropost

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
  
- before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+ before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers ]
  before_action :correct_user,
 only: [:edit, :update]
   before_action :admin_user,
@@ -19,6 +19,7 @@ only: :destroy
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
  end
 
   def new
@@ -49,6 +50,20 @@ only: :destroy
     else
 	render 'new'
     end
+  end
+
+   def following
+	@title = "Following"
+	@user = User.find(params[:id])
+	@users = @user.followed_users.paginate(page: params[:page])
+	render 'show_follow'
+   end
+
+  def followers
+	@title = "Followers"
+	@user = User.find(params[:id])
+	@users = @user.followers.paginate(page: params[:page])
+	render 'show_follow'
   end
 
   private
--- sample_app/spec/requests/authentication_pages_spec.rb
+++ sample_app/spec/requests/authentication_pages_spec.rb
@@ -45,6 +45,30 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       
+     describe "in the Relationships controller" do
+      describe "submitting to the create action" do
+        before { post relationships_path }
+	specify { expect(response).to redirect_to(signin_path) }
+      end
+
+      describe "submitting to the destroy action" do
+	before { delete relationship_path(1) }
+	specify { expect(response).to redirect_to(signin_path) }
+      end
+     end
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+	  before { post microposts_path }
+	  specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+           before { delete micropost_path(FactoryGirl.create(:micropost)) }
+           specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
@@ -67,6 +91,16 @@ describe "Authentication" do
           before { visit edit_user_path(user) }
           it { should have_title('Sign in') }
         end
+   
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+       end
         
         describe "visiting the user index" do
           before {visit users_path}
